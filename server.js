abdd2764dc49d33875e98e27ab9294c17af44167
rajpootGovind <govindrajpoot12000@gmail.com>
2024-11-21 19:41:59 +0530

    Database connection and server running successful

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const app = express()
+
+const port = process.env.PORT
+const url = process.env.DB_URL
+
+// Connection with database
+
+mongoose.connect(url)
+const db = mongoose.connection
+
+db.on("error", () => {
+    console.log(`error during connection with DB ${error}`)
+})
+
+db.once("open", () => {
+    console.log("DB connected successfully")
+
+    init()
+})
+
+const init = () => {
+    app.get("/print", () => {
+        console.log("initially connected")
+    })
+}
+
+
+// start the server
+
+app.listen(port, () => {
+    console.log(`server is running on port no. : ${port}`)
+})
